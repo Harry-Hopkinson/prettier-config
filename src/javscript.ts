5b7326d50786fbfa2912aa90b176e286f24577f2
Harry-Hopkinson <harryjhopkinson@gmail.com>
2022-02-09 18:05:55 +0000

    Revert to before prettier

--- src/javscript.ts
+++ src/javscript.ts
@@ -1,11 +1,10 @@
-import * as vscode from "vscode";
-import { activeEditor } from "./constants";
+import * as vscode from 'vscode';
+import { activeEditor } from './constants';
 
-export const javascript = activeEditor.edit(
-  (edit: { insert: (arg0: any, arg1: string) => void }) => {
+export const javascript = activeEditor.edit((edit: { insert: (arg0: any, arg1: string) => void; }) => {
     edit.insert(
-      new vscode.Position(0, 0),
-      `module.exports = {\n
+        new vscode.Position(0, 0),
+        `module.exports = {\n
             singleQuote: true,
             printWidth: 120,
             tabWidth: 4,
@@ -13,5 +12,4 @@ export const javascript = activeEditor.edit(
             endOfLine: 'auto'
         };`
     );
-  }
-);
+});
